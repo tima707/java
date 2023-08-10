75509ee27e0fc8fad123f212187190479466b57c
tima707 <141474835+tima707@users.noreply.github.com>
2023-08-10 16:26:04 +0500

    Create index.js

---
+++ homework/index.js
@@ -0,0 +1,28 @@
+let name = prompt('Name').toLowerCase().trim()
+
+if (name[0] === 'a') {
+    alert('Xorosho ' + name + ' dalshe') 
+
+    let age = prompt('Age')
+
+    if (age >= 20 && age <= 40) {
+        alert('Dalshe')
+    }
+
+    else {
+        alert('idi domoy')
+    }
+
+    let money = prompt('Money')
+
+    if (money <<=100) {
+        alert('Welcome rodnoy')
+    }
+
+    else {
+        alert('idi dimoy')
+    }
+
+    
+}
+
